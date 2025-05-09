48e6c764c159185c19a2c93ae9fd63de36f039dc
Enzo460 <enzo.braz@escola.pr.gov.br>
2025-05-09 13:57:24 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+# calculadora.py
+def main():
+    # Solicita ao usuário dois números
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    # Calcula a soma
+    soma = num1 + num2
+
+    # Exibe o resultado
+    print(f"A soma de {num1} e {num2} é {soma}")
+
+# Executa o programa
+if __name__ == "__main__":
+    main()
