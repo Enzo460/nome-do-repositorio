84dbef401ee06f3dc2431815fb11c3d049530609
Enzo460 <enzo.braz@escola.pr.gov.br>
2025-06-18 13:41:14 -0300

    Create verificaIdade.js

---
+++ verificaIdade.js
@@ -0,0 +1,11 @@
+// Verificador de Idade
+
+let idade = 17; // Altere este valor para testar outras idades
+
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
+
+https://onlinegdb.com/8yIrPuDi_
