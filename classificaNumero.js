1637111e6fef8d88d3f1fbc62ddae2d1293bc0dd
Enzo460 <enzo.braz@escola.pr.gov.br>
2025-06-18 13:48:03 -0300

    Create classificaNumero.js

---
+++ classificaNumero.js
@@ -0,0 +1,13 @@
+https://onlinegdb.com/vsu18N3en
+
+// Classificador de Número
+
+let numero = -5; // Altere este valor para testar diferentes casos
+
+if (numero > 0) {
+    console.log("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
