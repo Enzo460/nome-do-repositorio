881bb39688f68d12e8041c07894f27eb19a89c1d
Enzo460 <enzo.braz@escola.pr.gov.br>
2025-06-18 13:44:32 -0300

    Create verificaDia.js

---
+++ verificaDia.js
@@ -0,0 +1,11 @@
+// Verificador do Dia da Semana
+
+let dia = "Sábado"; // Altere para testar outros dias
+
+if (dia === "Sábado" || dia === "Domingo") {
+    console.log("É final de semana!");
+} else {
+    console.log("É dia de semana.");
+}
+
+https://onlinegdb.com/yz1e3zDQY
