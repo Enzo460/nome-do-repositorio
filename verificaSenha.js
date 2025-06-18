3f30e163da3cf63a41dead18a83c621ebd7aa7b3
Enzo460 <enzo.braz@escola.pr.gov.br>
2025-06-18 13:46:43 -0300

    Create verificaSenha.js

---
+++ verificaSenha.js
@@ -0,0 +1,12 @@
+// Verificador de Senha Simples
+
+let senha_correta = "12345";
+let senha_digitada = "12345"; // Altere para testar outras entradas
+
+if (senha_digitada === senha_correta) {
+    console.log("Login bem-sucedido!");
+} else {
+    console.log("Senha incorreta. Tente novamente.");
+}
+
+https://onlinegdb.com/ELqXMDoao
